dff69b8ea49f966205a6e4fbdaa72728c9e85ed1
Zaidi-husain <8802062406d@gmail.com>
2021-06-11 15:38:09 +0530

    added multiply.java

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply {
+    public static void main (String[] args){
+        int a = 60;
+        int b = 9;
+        System.out.println("The Result is" + (a * b));
+        }
+    }
+
