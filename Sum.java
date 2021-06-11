f73b1fa54f804b738e4c2a35d415ef3bcf0cb517
Zaidi-husain <8802062406d@gmail.com>
2021-06-11 15:17:17 +0530

    added message in Sum.java

--- Sum.java
+++ Sum.java
@@ -4,6 +4,6 @@ public class Sum {
     public static void main (String[] args){
     int a = 60;
     int b = 9;
-    System.out.println(a+b);
+    System.out.println("The sum is" + (a+b));
     }
 }
